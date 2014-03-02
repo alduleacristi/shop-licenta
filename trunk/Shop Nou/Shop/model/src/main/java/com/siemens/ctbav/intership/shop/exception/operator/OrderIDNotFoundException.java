2b9809a2ed847bf3ab3bf1e628a9b132f0c1d09b
iugadelia@ymail.com <iugadelia@ymail.com@1207e48a-9c9d-b76e-066b-47a31967cc94>
2014-03-02 13:31:46 +0000

    am adaugat niste clase

---
+++ trunk/Shop Nou/Shop/model/src/main/java/com/siemens/ctbav/intership/shop/exception/operator/OrderIDNotFoundException.java
@@ -0,0 +1,21 @@
+package com.siemens.ctbav.intership.shop.exception.operator;
+
+public class OrderIDNotFoundException extends Exception {
+
+	public OrderIDNotFoundException() {
+		super("id not found");
+		// TODO Auto-generated constructor stub
+	}
+
+	public OrderIDNotFoundException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public OrderIDNotFoundException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
---
+++ trunk/Shop Nou/Shop/model/src/main/java/com/siemens/ctbav/intership/shop/exception/operator/ProductException.java
@@ -0,0 +1,15 @@
+package com.siemens.ctbav.intership.shop.exception.operator;
+
+public class ProductException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public ProductException() {
+		super();
+	}
+
+	public ProductException(String message) {
+		super(message);
+	}
+
+}

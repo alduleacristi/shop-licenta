259d2f164ac610bb63b23bbf0a6c256557a67d12
iugadelia <iugadelia@ymail.com>
2014-03-12 11:40:28 +0000

    exceptie input...

---
+++ Shop Nou/Shop/model/src/main/java/com/siemens/ctbav/intership/shop/exception/operator/IncorrectInputException.java
@@ -0,0 +1,31 @@
+package com.siemens.ctbav.intership.shop.exception.operator;
+
+public class IncorrectInputException extends Exception {
+
+	public IncorrectInputException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public IncorrectInputException(String arg0, Throwable arg1, boolean arg2,
+			boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IncorrectInputException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IncorrectInputException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6692534200317850051L;
+
+}

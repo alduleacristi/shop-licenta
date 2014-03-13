8fa235e4e11289d6857854a267f9a0c4da712fa1
alduleacristi31@gmail.com <alduleacristi31@gmail.com@1207e48a-9c9d-b76e-066b-47a31967cc94>
2014-03-13 18:08:55 +0000

    Am adaugat un query pentru adresa userilor logati Am adaugat o exceptie pentru cazul in care nu se poate crea sistemul; de recomandari

---
+++ trunk/Shop Nou/Shop/model/src/main/java/com/siemens/ctbav/intership/shop/exception/client/NullRecommander.java
@@ -0,0 +1,15 @@
+package com.siemens.ctbav.intership.shop.exception.client;
+
+public class NullRecommander extends Exception{
+
+	private static final long serialVersionUID = 7642574949437585310L;
+	
+	public NullRecommander(){
+		
+	}
+	
+	public NullRecommander(String msg){
+		super(msg);
+	}
+
+}
